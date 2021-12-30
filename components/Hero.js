20f617be143585cb10e5a81b7d3a63d01f1e76a3
420punks <96455609+420punks@users.noreply.github.com>
2021-12-31 01:24:56 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ export default function Hero(props) {
 					</div>
 					<h1 className='mb-6'>{props.title}</h1>
 					{/* <h2 className='relative mb-4 text-4xl'>{props.tagline}</h2> */}
-					<Button url={props.opensea} text='Mint Here' />
+					<Button url={props.opensea} text='Mint Here | 830 AM PST , 1st January' />
 				</div>
 			</div>
 		</>
