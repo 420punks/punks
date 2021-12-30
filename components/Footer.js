d0c521531c5f0745de7c5ae8499071c1c9833ff4
420punks <96455609+420punks@users.noreply.github.com>
2021-12-31 01:20:15 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,12 +7,7 @@ export default function Footer() {
 			<footer className='flex flex-col items-center justify-center p-8'>
 				<Social />
 				<p className='mt-4'>© All rights reservered 420Punks</p>
-				<p className='text-xs opacity-50'>
-					Designed by{" "}
-					<a href='https://illusivemedia.co' target='_blank' rel='noreferrer'>
-						Illusive Media
-					</a>
-				</p>
+				
 			</footer>
 		</>
 	);
