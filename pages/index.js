b9f756d116eee5c96ad72816264429c9d399b437
420punks <96455609+420punks@users.noreply.github.com>
2021-12-31 21:08:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home({ about, roadmap, merch, project, mint }) {
 			<Hero
 				title='420 Punks'
 				tagline='Tagline'
-				opensea='https://opensea.io/collection/420pussdsdnksmint'
+				opensea='https://opensea.io/collection/420punksmint'
 			/>
 
 			<NFTSlider />
