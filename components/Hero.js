93a99511fde0257776ab620dc979d306c2669679
420punks <96455609+420punks@users.noreply.github.com>
2021-12-31 21:39:41 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,18 +4,18 @@ import Button from "./Button";
 import logo from "../public/logo.png";
 
 export default function Hero(props) {
-	return (
-		<>
-			<div className='hero lg:px-0 h-screen px-12 text-center'>
-				<div className='flex flex-col items-center justify-center h-full'>
-					<div className='max-w-[15rem] animate-hover filter drop-shadow-md'>
-						<Image src={logo} alt='420 Punks' />
-					</div>
-					<h1 className='mb-6'>{props.title}</h1>
-					{/* <h2 className='relative mb-4 text-4xl'>{props.tagline}</h2> */}
-					<Button url={props.opensea} text='Mint Here | 830 AM PST , 1st January' />
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className='hero lg:px-0 h-screen px-12 text-center'>
+                <div className='flex flex-col items-center justify-center h-full'>
+                    <div className='max-w-[15rem] animate-hover filter drop-shadow-md'>
+                        <Image src={logo} alt='420 Punks' />
+                    </div>
+                    <h1 className='mb-6'>{props.title}</h1>
+                    <h2 id="countdown" className='relative mb-4 text-4xl countdown'></h2>
+                    <Button url={props.opensea} text='Mint Here | 830 AM PST , 1st January' />
+                </div>
+            </div>
+        </>
+    );
 }
