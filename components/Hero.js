d43f9f4c15e709e5a1f6fb343fbbdf1b8913fc4a
420punks <96455609+420punks@users.noreply.github.com>
2021-12-31 21:56:17 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -13,7 +13,7 @@ export default function Hero(props) {
                     </div>
                     <h1 className='mb-6'>{props.title}</h1>
                     <h2 id="countdown" className='relative mb-4 text-4xl countdown'></h2>
-                    <Button url={props.opensea} text='Mint Here | 830 AM PST , 1st January' />
+                    <Button url={props.opensea} text='Mint Here' />
                 </div>
             </div>
         </>
